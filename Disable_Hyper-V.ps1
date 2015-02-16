237d425fdc5a526e02fa90070de5c1ca6cb82f06
Josh <josh.quintus@gmail.com>
2015-02-16 10:24:45 -0500

    Update Disable_Hyper-V.ps1

--- Disable_Hyper-V.ps1
+++ Disable_Hyper-V.ps1
@@ -1,5 +1,6 @@
 # This script makes a copy of the current boot record and disables Hyper-V
 # The next time the computer is restarted you can elect to run without Hyper-V
+# To run a powershell script: powershell -file Disable_Hyper-v.ps1
  
 $output  = invoke-expression 'bcdedit /copy "{current}" /d "Hyper-V Disabled"'
 $output -match '{.*}'
