48b97da3c966517bb02a5c33353000462967bb22
Josh <josh.quintus@gmail.com>
2015-02-21 16:10:20 -0500

    Add all the pinned tabs in order

--- spikes.ps1
+++ spikes.ps1
@@ -1,1 +1,9 @@
-Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" 
+# https://github.com/chocolatey/chocolatey/blob/master/src/helpers/functions/Install-ChocolateyPinnedTaskBarItem.ps1
+
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\vim\vim74\gvim.exe"
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files\ConEmu\ConEmu64.exe"
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Trillian\trillian.exe"
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\KeePass Password Safe 2\KeePass.exe"
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files\MPC-HC\mpc-hc64.exe"
