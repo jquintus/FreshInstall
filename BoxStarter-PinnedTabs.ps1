582f2c1ce0b02bfd82bbf623adb1a60f2af041ec
jquintus <josh.quintus@live.com>
2015-02-21 16:23:18 -0500

    add  -ErrorAction SilentlyContinue flag

--- BoxStarter-PinnedTabs.ps1
+++ BoxStarter-PinnedTabs.ps1
@@ -1,11 +1,11 @@
 # https://github.com/chocolatey/chocolatey/blob/master/src/helpers/functions/Install-ChocolateyPinnedTaskBarItem.ps1
 
-Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
-Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\vim\vim74\gvim.exe"
-Install-ChocolateyPinnedTaskBarItem "C:\Program Files\ConEmu\ConEmu64.exe"
-Install-ChocolateyPinnedTaskBarItem "C:\Program Files\paint.net\PaintDotNet.exe"
-Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Trillian\trillian.exe"
-Install-ChocolateyPinnedTaskBarItem "C:\WINDOWS\explorer.EXE"
-Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\KeePass Password Safe 2\KeePass.exe"
-Install-ChocolateyPinnedTaskBarItem "C:\Program Files\MPC-HC\mpc-hc64.exe"
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" -ErrorAction SilentlyContinue
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -ErrorAction SilentlyContinue
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\vim\vim74\gvim.exe" -ErrorAction SilentlyContinue
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files\ConEmu\ConEmu64.exe" -ErrorAction SilentlyContinue
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files\paint.net\PaintDotNet.exe" -ErrorAction SilentlyContinue
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Trillian\trillian.exe" -ErrorAction SilentlyContinue
+Install-ChocolateyPinnedTaskBarItem "C:\WINDOWS\explorer.EXE" -ErrorAction SilentlyContinue
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\KeePass Password Safe 2\KeePass.exe" -ErrorAction SilentlyContinue
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files\MPC-HC\mpc-hc64.exe" -ErrorAction SilentlyContinue
