a8680adeb574dc67708c1af561f7132343a5d3ed
Josh <josh.quintus@gmail.com>
2015-02-21 16:04:30 -0500

    Use the correct version of VS

--- spikes.ps1
+++ spikes.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" 
+Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" 
