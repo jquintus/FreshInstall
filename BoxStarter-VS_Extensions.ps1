96673bb3200739f42c4db8bd51a63a379e045ffd
Josh <josh.quintus@gmail.com>
2016-10-05 16:31:51 -0400

    Update BoxStarter-VS_Extensions.ps1

--- BoxStarter-VS_Extensions.ps1
+++ BoxStarter-VS_Extensions.ps1
@@ -3,14 +3,14 @@
 Install-ChocolateyVsixPackage VSOpenIn https://visualstudiogallery.msdn.microsoft.com/aaff387c-9cf9-4555-93b1-3572930f2cae/file/154471/1/VsOpenIn.vsix
 Install-ChocolateyVsixPackage CodeMaid http://visualstudiogallery.msdn.microsoft.com/76293c4d-8c16-4f4a-aee6-21f83a571496/file/9356/28/CodeMaid_v0.7.4.vsix
 Install-ChocolateyVsixPackage NunitTestAdapter http://visualstudiogallery.msdn.microsoft.com/6ab922d0-21c0-4f06-ab5f-4ecd1fe7175d/file/66177/15/NUnitVisualStudioTestAdapter-1.2.vsix
-Install-ChocolateyVsixPackage ProductifyPowerTools2013 http://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/4/ProPowerTools.vsix
 Install-ChocolateyVsixPackage vsspellchecker http://visualstudiogallery.msdn.microsoft.com/a23de100-31a1-405c-b4b7-d6be40c3dfff/file/104494/5/VSSpellCheckerPackage.vsix
 Install-ChocolateyVsixPackage VSCommandsForVisualStudio http://visualstudiogallery.msdn.microsoft.com/c6d1c265-7007-405c-a68b-5606af238ece/file/106247/18/SquaredInfinity.VSCommands.VS12.vsix
-
-#TO ADD
-# ProductifyPowerTools2015
+Install-ChocolateyVsixPackage ProductivityPowerTools2015 https://visualstudiogallery.msdn.microsoft.com/34ebc6a2-2777-421d-8914-e29c1dfa7f5d/file/169971/3/ProPowerTools.vsix
+Install-ChocolateyVsixPackage ProductivityPowerToolsForF# https://visualstudiogallery.msdn.microsoft.com/136b942e-9f2c-4c0b-8bac-86d774189cff/file/124201/38/FSharpVSPowerTools.vsix
+# TO ADD
 # Open on GitHub
 
 
 # Removed
 # Install-ChocolateyVsixPackage SlowCheetahXml http://visualstudiogallery.msdn.microsoft.com/69023d00-a4f9-4a34-a6cd-7e854ba318b5/file/55948/26/SlowCheetah.vsix
+# Install-ChocolateyVsixPackage ProductifyPowerTools2013 http://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/4/ProPowerTools.vsix
