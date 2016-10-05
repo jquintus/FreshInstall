bf403f69330b263eb142a00c5ca5461242e17bf0
Josh <josh.quintus@gmail.com>
2016-10-05 15:08:16 -0400

    Remove vs extensions.
    they are already in a separate file

--- BoxStarter-Setup.ps1
+++ BoxStarter-Setup.ps1
@@ -27,7 +27,6 @@ Update-ExecutionPolicy Unrestricted
 Enable-MicrosoftUpdate
 Install-WindowsUpdate -acceptEula -getUpdatesFromMS
 
-
 # Must Have Toys
 cinst -y GoogleChrome
 cinst -y KickAssVim
@@ -36,17 +35,6 @@ cinst -y greenshot # Keep this before Dropbox to make sure we get the print scre
 cinst -y keepass
 cinst -y cmder.portable
 # cinst -y jquintus.ConEmuConfig #TO DO:  Make this work
-
-# Big Toys
-#cinst -y VisualStudio2013Ultimate Blend SQL Win8SDK WindowsPhone80 WebTools
-#cinst -y xamarin-visualstudio
-#cinst -y visualstudio2013-webessentials.vsix
-#Install-ChocolateyVsixPackage CodeMaid http://visualstudiogallery.msdn.microsoft.com/76293c4d-8c16-4f4a-aee6-21f83a571496/file/9356/28/CodeMaid_v0.7.4.vsix
-#Install-ChocolateyVsixPackage NunitTestAdapter http://visualstudiogallery.msdn.microsoft.com/6ab922d0-21c0-4f06-ab5f-4ecd1fe7175d/file/66177/15/NUnitVisualStudioTestAdapter-1.2.vsix
-#Install-ChocolateyVsixPackage ProductifyPowerTools2013 http://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/4/ProPowerTools.vsix
-#Install-ChocolateyVsixPackage SlowCheetahXml http://visualstudiogallery.msdn.microsoft.com/69023d00-a4f9-4a34-a6cd-7e854ba318b5/file/55948/26/SlowCheetah.vsix
-#Install-ChocolateyVsixPackage vsspellchecker http://visualstudiogallery.msdn.microsoft.com/a23de100-31a1-405c-b4b7-d6be40c3dfff/file/104494/5/VSSpellCheckerPackage.vsix
-#Install-ChocolateyVsixPackage VSCommandsForVisualStudio http://visualstudiogallery.msdn.microsoft.com/c6d1c265-7007-405c-a68b-5606af238ece/file/106247/18/SquaredInfinity.VSCommands.VS12.vsix
 
 # Less Important Toys
 cinst -y fiddler4
